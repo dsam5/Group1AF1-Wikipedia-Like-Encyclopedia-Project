819c3b502b28ecfbe402a4fb47b5fd3273ed76f2
dsam5 <98347052+dsam5@users.noreply.github.com>
2024-02-24 21:51:52 +0000

    Updated

--- edit_page.js
+++
@@ -1,8 +0,0 @@
-// editPage.js
-window.onload = function() {
-  var title = localStorage.getItem('title');
-  var text = localStorage.getItem('text');
-
-  document.getElementById('title').value = title;
-  document.getElementById('text').value = text;
-};
